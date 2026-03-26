865c9e84948e83231e02f21a353d28146b57e22a
boutiquevert-coder <boutiquevert@gmail.com>
2026-03-26 16:55:33 +0200

    Change time filter from 1 hour to 1 day

--- usdc_whale_tracker.sql
+++ usdc_whale_tracker.sql
@@ -1,11 +1,11 @@
 /* USDC Whale Tracker - Ethereum Mainnet */
 SELECT 
-    tr.from AS "Sender", 
-    tr.to AS "Receiver", 
+    tr."from" AS "Sender",
+    tr."to" AS "Receiver",
     tr.value / 1e6 AS "Amount_USDC",
     tr.evt_block_time AS "Time"
 FROM erc20_ethereum.evt_Transfer tr
 WHERE tr.contract_address = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48
 AND tr.value / 1e6 >= 1000000
-AND tr.evt_block_time > now() - interval '1 hour'
+AND tr.evt_block_time > now() - interval '1' day
 ORDER BY tr.value DESC;
